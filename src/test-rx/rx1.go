fce3bd669434fb5e5b4dee28b9d8134bfcb70f26
Bair Dev <trial.and.error@gmx.net>
2017-10-27 18:02:32 +0200

    OK, really not compiling

--- src/test-rx/rx1.go
+++ src/test-rx/rx1.go
@@ -1,4 +1,42 @@
 package main
+
+// DOES NOT COMPILE!
+
+// go build -o /home/mbuechs/work/tryit/rx_go/bin/rx1 rx1.go
+// rx1.go:5:2: cannot find package "google.golang.org/api/iterator" in any of:
+//         /usr/local/go/src/google.golang.org/api/iterator (from $GOROOT)
+//         /home/mbuechs/work/go/src/google.golang.org/api/iterator (from $GOPATH)
+
+// go env
+// GOARCH="amd64"                                                                                                                                       
+// GOBIN="/home/mbuechs/work/go/bin"                                                                                                                    
+// GOEXE=""
+// GOHOSTARCH="amd64"
+// GOHOSTOS="linux"
+// GOOS="linux"
+// GOPATH="/home/mbuechs/work/go"
+// GORACE=""
+// GOROOT="/usr/local/go"
+// GOTOOLDIR="/usr/local/go/pkg/tool/linux_amd64"
+// GCCGO="gccgo"
+// CC="gcc"
+// GOGCCFLAGS="-fPIC -m64 -pthread -fmessage-length=0 -fdebug-prefix-map=/tmp/go-build327165865=/tmp/go-build -gno-record-gcc-switches"
+// CXX="g++"
+// CGO_ENABLED="1"
+// CGO_CFLAGS="-g -O2"
+// CGO_CPPFLAGS=""
+// CGO_CXXFLAGS="-g -O2"
+// CGO_FFLAGS="-g -O2"
+// CGO_LDFLAGS="-g -O2"
+// PKG_CONFIG="pkg-config"
+
+// echo $PATH
+// /home/mbuechs/.nvm/versions/node/v6.10.0/bin:/home/mbuechs/bin:/home/mbuechs/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/local/go/bin
+
+// https://github.com/golang/go/wiki/GOPATH
+// https://stackoverflow.com/questions/21001387/how-do-i-set-the-gopath-environment-variable-on-ubuntu-what-file-must-i-edit
+// https://stackoverflow.com/questions/24306183/can-someone-explain-why-gopath-is-convenient-and-how-it-should-be-used-in-genera?noredirect=1&lq=1
+
 import (
 	"fmt"
 	"errors"
