0ac05d548046878cdad960769f38f2bc6ce89f94
Bair Dev <trial.and.error@gmx.net>
2017-12-01 15:18:20 +0100

    trying to clean up (put repo in GOPATH folder)

---
+++ test-rx/rx1.go
@@ -0,0 +1,72 @@
+package main
+
+// go env
+
+// GOPATH="/home/mbuechs/work/go"
+// GOROOT="/usr/local/go"
+// PKG_CONFIG="pkg-config"
+
+(
+	"fmt"
+	"github.com/reactivex/rxgo/observer"
+	"github.com/reactivex/rxgo/observable"
+	"github.com/reactivex/rxgo/handlers"
+)
+
+// former import statements
+// "errors"
+// "google.golang.org/api/iterator"
+
+func watchSimple() {
+	watcher := observer.Observer{
+
+		// Register a handler function for every next available item.
+		NextHandler: func(item interface{}) {
+			fmt.Printf("Processing: %v\n", item)
+		},
+
+		// Register a handler for any emitted error.
+		ErrHandler: func(err error) {
+			fmt.Printf("Encountered error: %v\n", err)
+		},
+
+		// Register a handler when a stream is completed.
+		DoneHandler: func() {
+			fmt.Println("Done!")
+		},
+	}
+
+	it, _ := iterable.New([]interface{}{1, 2, 3, 4, errors.New("bang"), 5})
+	source := observable.From(it)
+	sub := source.Subscribe(watcher)
+
+	// wait for the channel to emit a Subscription
+	<- sub
+}
+
+func main() {
+
+	score := 9
+
+	onNext := handlers.NextFunc(func(item interface{}) { // http://jordanorelli.com/post/32665860244/how-to-use-interfaces-in-go
+		// classical type assertion https://nanxiao.gitbooks.io/golang-101-hacks/content/posts/type-assertion-and-type-switch.html
+		if num, ok := item.(int); ok {
+			score += num
+			fmt.Printf("Incoming: %d\n", num)
+		}
+	})
+
+	onDone := handlers.DoneFunc(func() {
+		score *= 2
+	})
+
+	watcher := observer.New(onNext, onDone)
+
+	// Create an `Observable` from a single item and subscribe to the observer.
+	sub := observable.Just(1).Subscribe(watcher)
+	<- sub
+
+	fmt.Printf("Score has become: %d\n", score) // 20
+
+	watchSimple()
+}
